9eada63a9c0f308400220e5b4d200227c8de60d4
Rodlyo <rodclases24@gmail.com>
2025-05-02 09:46:25 -0500

    ModificacionDB

--- crud-vendedores/config/db.js
+++ crud-vendedores/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "tramway.proxy.rlwy.net",
+  host: "crossover.proxy.rlwy.net",
   user: "root",
-  password: "kQFQPMPIkAojatyUTITqzDGeSfpWuYfg",
+  password: "sgxFgmVvmWGposMOQryeYGxHGCcSkDZM",
   database: "railway",
-  port: 35910,
+  port: 40949,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
